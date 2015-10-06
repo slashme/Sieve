b4e5e85606e75f56da975d222f586e92f2204b06
David Richfield <davidrichfield@gmail.com>
2015-10-06 23:50:54 +0200

    timing is off, add comment.

--- HilbertCode.py
+++ HilbertCode.py
@@ -108,7 +108,7 @@ for i in range(len(poslist)):
     outfile.write(tempstring)
     outfile.write('        <animate attributeName="opacity" attributeType="CSS"\n')
     #Rough prime counting function is n/ln(n)
-    tempstring='        to="1" begin="%ds" dur="1s" fill="freeze" />\n' % (math.ceil(1.2*iterations/math.log(iterations)))
+    tempstring='        to="1" begin="%ds" dur="1s" fill="freeze" />\n' % (math.ceil(1.2*iterations/math.log(iterations))) #FIXME - this is still wrong. See OEIS of 1,2,4,6,11,18,31,54,97 though.
     outfile.write(tempstring)
     outfile.write('    </text>\n')
 
