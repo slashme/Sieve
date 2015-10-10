01104bb5b823eab66303ef1baecd7c2aa1d0247f
David Richfield <davidrichfield@gmail.com>
2015-10-10 21:07:29 +0200

    rm debug; better comments

--- hexgrid.py
+++ hexgrid.py
@@ -1,4 +1,5 @@
 import math, operator
+#Static defs
 #Number of rings
 rings=3
 #Need sin(pi/3) for hexagon corner:
@@ -12,22 +13,18 @@ moves=[
     [ 0.5,-sp3], #right down
     [ 1.0, 0.0]  #right
     ]
+
+#Create the spiral:
 #Start in the middle
 points=[[0,0]]
-print(str(points[-1])) #debug
+#Now add points while spiraling outward.
 for i in range(rings):
-  #move right one step:
+  #move right one step to go to next ring:
   points.append(map(operator.add, points[-1], moves[-1]))
-  print("right ring %d" %i) #debug
-  print("[%.3f,%.3f]" %(points[-1][0], points[-1][1])) #debug
   #First side is short:
   for k in range(i):
-    print("right up ring %d cell %d" %(i,k)) #debug
     points.append(map(operator.add, points[-1], moves[0]))
-    print("[%.3f,%.3f]" %(points[-1][0], points[-1][1])) #debug
-  #Do the rest of the sides:
+  #Do the rest of the sides at full length:
   for j in range(1,len(moves)):
     for k in range(i+1):
-      print("ring %d side %d cell %d" %(i,j,k)) #debug
       points.append(map(operator.add, points[-1], moves[j]))
-      print("[%.3f,%.3f]" %(points[-1][0], points[-1][1])) #debug
