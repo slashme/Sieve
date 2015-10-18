7d3c9789e320310a8a2449da19a9be1a09ee9802
David Richfield <davidrichfield@gmail.com>
2015-10-18 18:43:39 +0200

    shape is right, fix start position.
    The bugs with the curve shape are now fixed, but the starting position
    is still wrong.

--- dragon.py
+++ dragon.py
@@ -1,4 +1,6 @@
 import math
+#Diagram size
+diagsz=500.0
 #Static SVG file name for now:
 svgfilename="/home/david/Pictures/Sieve/dragonOutput.svg"
 # Open svg file for writing:
@@ -7,8 +9,7 @@ outfile=open(svgfilename,'w')
 outfile.write('<?xml version="1.0" encoding="UTF-8" standalone="no"?>\n')
 outfile.write('<svg xmlns:svg="http://www.w3.org/2000/svg"\n')
 outfile.write('xmlns="http://www.w3.org/2000/svg" version="1.1"\n')
-#Make 1000px diagram:
-outfile.write('width="1000" height="1000" >\n')
+outfile.write('width="%.2f" height="%.2f" >\n' % (diagsz, diagsz))
 
 #How many iterations of the curve to draw:
 iterations=4
@@ -34,22 +35,24 @@ segments=[[None,None,0],[None,None,1]]
 for j in range(iterations):
   tempdir=firstdir #Start with the saved first direction, will iterate.
   for i in range(2**(j+1)+1):
-    if i%2:
+    if i%2: #if i is odd
       segments.insert(i,[None,None,(i/2)%2])
     if i:
       if not segments[i-1][2] and not segments[i][2]: tempdir=(tempdir-2)%8
       if segments[i-1][2] and segments[i][2]: tempdir=(tempdir+2)%8
     segments[i][0]=directions[tempdir]
     segments[i][1]=steplength
-  radius=math.sqrt(sum([x**2 for x in directions[firststep]]))*steplength
+  radius=1/(2**(j*0.5+1))
+  print("radius " + str(radius))
   outfile.write('<path style="fill:none;stroke:black;stroke-width:0.5" \n')
-  outfile.write('d="M %.4f,%.4f\n' % tuple([x*radius**2/2*1000 for x in directions[firststep]]))
+  outfile.write('d="M %.4f,%.4f\n' % tuple([x*steplength*diagsz for x in directions[firststep]]))
   for i in range(len(segments)):
-    outfile.write('   a %.4f,%.4f 0 0,%d %.4f,%.4f\n' % (radius*1000, radius*1000, segments[i][2], segments[i][0][0]*steplength*1000, segments[i][0][1]*steplength*1000))
+    outfile.write('   a %.4f,%.4f 0 0,%d %.4f,%.4f\n' % (radius*diagsz, radius*diagsz, segments[i][2], segments[i][0][0]*steplength*diagsz, segments[i][0][1]*steplength*diagsz))
   outfile.write('"\n/>\n')
   firstdir =(firstdir +1)%8
   firststep=(firststep+1)%8
-  if j%2:
+  if (j)%2:
     steplength/=2
-  print(str(steplength))
+  print("steplength "+str(steplength))
+  print("iteration "+str(j))
 outfile.write('</svg>')
